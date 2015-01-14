fde4da3958fd6ae2be97d8ec15882e2c35ea24eb
exsemt <exsemt@gmail.com>
2015-01-14 11:41:00 +0100

    wip photos

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,26 @@
 'use strict';
 /*jshint esnext: true */
 
+/*
 import MainCtrl from './main/main.controller';
 import NavbarCtrl from '../components/navbar/navbar.controller';
+*/
+
+import PhotosCtrl from './photos/photos.controller';
 
 angular.module('rdriveNg', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute', 'ngMaterial'])
+/*
   .controller('MainCtrl', MainCtrl)
   .controller('NavbarCtrl', NavbarCtrl)
-  
+  */
+
+  .controller('PhotosCtrl', PhotosCtrl)
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'app/photos/photos_table.html',
+        controller: 'PhotosCtrl'
       })
       .otherwise({
         redirectTo: '/'
