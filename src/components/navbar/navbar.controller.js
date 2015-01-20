2be4eb5cf147cf2f4397f435e0ebaf8426436bd4
exsemt <exsemt@gmail.com>
2015-01-20 22:29:35 +0100

    (WIP) navbar

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -4,6 +4,28 @@
 class NavbarCtrl {
   constructor ($scope) {
     $scope.date = new Date();
+
+    $scope.menuItems = [
+        {
+          name: 'Home',
+          url:  '/',
+          title: 'Welcome to our Website'
+        },
+        {
+          name: 'Photos',
+          url:  '/photos',
+          title: 'Your photos'
+        },
+        {
+          name:   'Contact',
+          url:    '/contact',
+          title:  'Get in touch with us'
+        }
+    ];
+
+    $scope.isActive = function (viewLocation) {
+      return viewLocation === $location.path();
+    };
   }
 }
 
